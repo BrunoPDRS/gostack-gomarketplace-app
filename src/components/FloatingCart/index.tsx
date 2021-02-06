e71031320e5a2f85245fd34e9a631ac5c4164776
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-02-06 13:43:28 -0300

    Adding all functionalities (approved in tests)

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useMemo } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
 
@@ -24,15 +24,19 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((acc, prod) => {
+      const productsSubtotal = prod.price * prod.quantity;
 
-    return formatValue(0);
+      return acc + productsSubtotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((acc, prod) => acc + prod.quantity, 0);
 
-    return 0;
+    return total;
   }, [products]);
 
   return (
